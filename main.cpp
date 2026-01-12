9ba6d1be59f256088db92ac09cec66c3d8e64f0f
GarryChen-site <chenymj23@gmail.com>
2026-01-12 19:43:42 +0900

    sdp signaling

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-#include "TinyJson.hpp"
-#include <api/peer_connection_interface.h>
-#include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+// WebRTC related headers
+#include <api/create_peerconnection_factory.h>
+#include <api/peer_connection_interface.h>
 #include <rtc_base/ssl_adapter.h>
+#include <rtc_base/thread.h>
 #include <system_wrappers/include/field_trial.h>
 
+// --- Observer Classes ---
+class Wrapper;
 class PeerConnectionObserverProcy : public webrtc::PeerConnectionObserver {
+  Wrapper &parent;
+
 public:
+  PeerConnectionObserverProcy(Wrapper &p) : parent(p) {}
   void OnSignalingChange(
       webrtc::PeerConnectionInterface::SignalingState new_state) override {
-    std::cout << "SignalingState Change: " << new_state
+    std::cout << "[PCO] SignalingState Change: " << new_state
               << " on thread: " << std::this_thread::get_id() << std::endl;
   }
   void OnAddStream(
@@ -25,25 +36,51 @@ public:
 
   void OnDataChannel(
       rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel) override {
-    std::cout << "DataChannel received!" << std::endl;
+    std::cout << "[PCO] DataChannel received!" << std::endl;
   }
 
   void OnRenegotiationNeeded() override {
-    std::cout << "Renegotiation needed!" << std::endl;
+    std::cout << "[PCO] Renegotiation needed!" << std::endl;
   }
 
   void OnIceConnectionChange(
       webrtc::PeerConnectionInterface::IceConnectionState new_state) override {
-    std::cout << "IceConnectionState Change: " << new_state << std::endl;
+    std::cout << "[PCO] IceConnectionState Change: " << new_state << std::endl;
   }
   void OnIceGatheringChange(
       webrtc::PeerConnectionInterface::IceGatheringState new_state) override {
-    std::cout << "IceGatheringState Change: " << new_state << std::endl;
+    std::cout << "[PCO] IceGatheringState Change: " << new_state << std::endl;
   }
   void OnIceCandidate(const webrtc::IceCandidateInterface *candidate) override {
-    std::cout << "IceCandidate found!" << std::endl;
+    std::cout << "[PCO] IceCandidate found!" << std::endl;
   }
 };
+
+class CreateSessionDescriptionObserverProxy
+    : public webrtc::CreateSessionDescriptionObserver {
+  Wrapper &parent;
+
+public:
+  CreateSessionDescriptionObserverProxy(Wrapper &p) : parent(p) {}
+  void OnSuccess(webrtc::SessionDescriptionInterface *desc) override;
+  void OnFailure(webrtc::RTCError error) override {
+    std::cerr << "[CSDO] Failure: " << error.message() << std::endl;
+  }
+};
+
+class SetSessionDescriptionObserverProxy
+    : public webrtc::SetSessionDescriptionObserver {
+public:
+  SetSessionDescriptionObserverProxy() {}
+  void OnSuccess() override {
+    std::cout << "[SSDO] Success (Description Set)" << std::endl;
+  }
+  void OnFailure(webrtc::RTCError error) override {
+    std::cerr << "[SSDO] Failure: " << error.message() << std::endl;
+  }
+};
+
+// --- Wrapper Implementation ---
 
 class Wrapper {
 public:
@@ -51,6 +88,20 @@ public:
   std::unique_ptr<rtc::Thread> worker_thread;
   std::unique_ptr<rtc::Thread> signaling_thread;
   rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> pcf;
+  rtc::scoped_refptr<webrtc::PeerConnectionInterface> pc;
+
+  // Observers
+  PeerConnectionObserverProcy pco{*this};
+  rtc::scoped_refptr<CreateSessionDescriptionObserverProxy> csdo;
+  rtc::scoped_refptr<SetSessionDescriptionObserverProxy> ssdo;
+
+  std::function<void(const std::string &)> on_sdp_callback;
+
+  Wrapper() {
+    csdo =
+        new rtc::RefCountedObject<CreateSessionDescriptionObserverProxy>(*this);
+    ssdo = new rtc::RefCountedObject<SetSessionDescriptionObserverProxy>();
+  }
 
   void init() {
     std::cout << "Initializing WebRTC..." << std::endl;
@@ -81,14 +132,83 @@ public:
     std::cout << "PeerConnectionFactory initialized successfully." << std::endl;
   }
 
+  void create_offer() {
+    std::cout << "Creating Offer..." << std::endl;
+    webrtc::PeerConnectionInterface::RTCConfiguration config;
+
+    pc = pcf->CreatePeerConnection(config, nullptr, nullptr, &pco);
+    if (!pc) {
+      std::cerr << "Failed to create PeerConnection!" << std::endl;
+      return;
+    }
+
+    pc->CreateOffer(csdo,
+                    webrtc::PeerConnectionInterface::RTCOfferAnswerOptions());
+  }
+
+  void create_answer(const std::string &remote_sdp) {
+    std::cout << "Creating Answer..." << std::endl;
+    webrtc::PeerConnectionInterface::RTCConfiguration config;
+    pc = pcf->CreatePeerConnection(config, nullptr, nullptr, &pco);
+
+    webrtc::SdpParseError error;
+    webrtc::SessionDescriptionInterface *session_desc =
+        webrtc::CreateSessionDescription("offer", remote_sdp, &error);
+
+    if (!session_desc) {
+      std::cerr << "Failed to parse remote SDP: " << error.description
+                << std::endl;
+      return;
+    }
+
+    pc->SetRemoteDescription(ssdo, session_desc);
+    pc->CreateAnswer(csdo,
+                     webrtc::PeerConnectionInterface::RTCOfferAnswerOptions());
+  }
+
+  void set_remote_answer(const std::string &remote_rdp) {
+    std::cout << "Setting Remote Answer..." << std::endl;
+    webrtc::SdpParseError error;
+    webrtc::SessionDescriptionInterface *session_desc =
+        webrtc::CreateSessionDescription("answer", remote_rdp, &error);
+
+    if (!session_desc) {
+      std::cerr << "Failed to parse remote Answer: " << error.description
+                << std::endl;
+      return;
+    }
+    pc->SetRemoteDescription(ssdo, session_desc);
+  }
+
+  void on_local_sdp_ready(webrtc::SessionDescriptionInterface *desc) {
+    std::string sdp;
+    desc->ToString(&sdp);
+    pc->SetLocalDescription(ssdo, desc);
+    if (on_sdp_callback) {
+      on_sdp_callback(sdp);
+    }
+  }
+
   void cleanup() {
     std::cout << "Cleaning up Wrapper..." << std::endl;
+    if (pc) {
+      pc->Close();
+    }
+    pc = nullptr;
     pcf = nullptr;
     network_thread->Stop();
     worker_thread->Stop();
     signaling_thread->Stop();
   }
 };
+
+void CreateSessionDescriptionObserverProxy::OnSuccess(
+    webrtc::SessionDescriptionInterface *desc) {
+  std::cout << "[CSDO] Success (Description Created)" << std::endl;
+  parent.on_local_sdp_ready(desc);
+}
+
+// --- Main Program ---
 
 int main(int argc, char *argv[]) {
 
@@ -100,13 +220,45 @@ int main(int argc, char *argv[]) {
   Wrapper rtc_wrapper;
   rtc_wrapper.init();
 
-  std::cout << "WebRTC Factory and Threads are live. Type 'quit' to exit!"
-            << std::endl;
+  rtc_wrapper.on_sdp_callback = [](const std::string &sdp) {
+    std::cout << "\n--- SDP START ---" << std::endl;
+    std::cout << sdp;
+    std::cout << "--- SDP END ---\n" << std::endl;
+  };
+
+  std::cout
+      << "Commands: 'sdp1' (Offer), 'sdp2' (Answer), 'sdp3' (SetAnswer), 'quit'"
+      << std::endl;
 
   std::string line;
+  std::string command;
+  std::string parameter;
+  bool collecting_param = false;
+
   while (std::getline(std::cin, line)) {
-    if (line == "quit")
-      break;
+    if (!collecting_param) {
+      if (line == "quit")
+        break;
+      if (line == "sdp1") {
+        rtc_wrapper.create_offer();
+      } else if (line == "sdp2" || line == "sdp3") {
+        command = line;
+        collecting_param = true;
+        parameter = "";
+        std::cout << "Pated SDP then type ';' on a new line:" << std::endl;
+      }
+    } else {
+      if (line == ";") {
+        collecting_param = false;
+        if (command == "sdp2") {
+          rtc_wrapper.create_answer(parameter);
+        } else if (command == "sdp3") {
+          rtc_wrapper.set_remote_answer(parameter);
+        }
+      } else {
+        parameter += line + "\n";
+      }
+    }
   }
 
   rtc_wrapper.cleanup();
